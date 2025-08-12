c255a24ca63dd6f191f80238bcd223e484e3452e
Kissg22 <103967397+Kissg22@users.noreply.github.com>
2025-08-12 20:37:29 +0000

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // user site-hoz marad '/'
+  base: '/kissg.github.io/',   // <-- EZ A LÉNYEG, mert project site
 })
