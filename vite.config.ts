18ef162d3f3b6e79998dc35d84199b3ae16f7533
Kiss Gábor <103967397+Kissg22@users.noreply.github.com>
2025-08-13 16:45:16 +0000

    up

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/kissg/',     // <-- KELL a záró perjel
+  // EZ A SOR A LÉNYEG: megadjuk a repository nevét, mint alap útvonal.
+  base: '/kissg/', 
 })
